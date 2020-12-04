fde971aa3d5459ad19ac28cbd93038ac6f01869b
Evgeny Lyozin <evgenylyozindev@gmail.com>
2020-12-04 15:51:53 +0300

    added subscription options

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const { PrismaClient } = require('@prisma/client')
 const { GraphQLServer } = require('graphql-yoga')
+const { PubSub } = require('graphql-yoga')
 
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
+
+const pubsub = new PubSub()
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
-  Link
+  Link,
+  Vote,
 }
 
 const prisma = new PrismaClient()
@@ -22,6 +29,7 @@ const server = new GraphQLServer({
       return {
         ...request,
         prisma,
+        pubsub,
       }
     },
 })
